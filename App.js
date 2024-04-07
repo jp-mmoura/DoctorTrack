34351c0981bff65b89330db9ac321330496df4af
João Pedro <133026499+jp-mmoura@users.noreply.github.com>
2024-04-07 14:49:32 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,83 +1,12 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput,} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {Routes} from './src/routes'
 
 export default function App() {
-  const [text, setText] = useState('');
-  const [selectedActivity, setSelectedActivity] = useState();
-  const onChangeText = (value) => {
-    setText(value);
-  };
-
-  const specialties = [
-    { label: "Clínica Médica", value: "clinica medica" },
-    { label: "Cirurgia", value: "cirurgia" },
-    { label: "Pediatria", value: "pediatria" },
-    { label: "Ginecologia e Obstetrícia", value: "ginecologia e obstetricia" },
-    { label: "Psiquiatria", value: "psiquiatria" },
-    { label: "Medicina de Família e Comunidade", value: "medicina de familia e comunidade" },
-  ];
-
-  const specialtiesArray = [
-    { label: "Selecione a Especialidade", value: "selecione a especialidade", enabled: false, style: { color: "gray" } },
-    ...specialties.map((specialty, index) => specialty),
-  ];
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("./src/assets/logo-doctrack.png")}
-        style={styles.logo}
-      />
-
-      <Image
-        source={require("./src/assets/QRCode.png")}
-        style={styles.logo}
-      />
-
-<Picker
-        style={styles.pickerComponent}
-        selectedValue={selectedActivity}
-        onValueChange={(itemValue, itemIndex) => setSelectedActivity(itemValue)}
-      >
-        {specialtiesArray.map((specialty, index) => (
-          <Picker.Item label={specialty.label} value={specialty.value} key={index} enabled={index > 0} style={index === 0 ? { color: "gray" } : {}} />
-        ))}
-      </Picker>
-        <TextInput
-  style={{ height: 200, width: 200, ...styles.textinput, fontWeight: 'normal'}} 
-  placeholder='Atividade'
-  onChangeText={onChangeText}
-  value={text}
-  multiline={true}
-  numberOfLines={10}
-/>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F3F3FF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    marginBottom: 60,
-  },
-  textinput: {
-    width: '80%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingLeft: 10,
-    marginTop: 20,
-    color: 'black',
-    fontWeight: 'bold'
-  },
-  pickerComponent: {
-    width: 350,
-  },
-});
---
+++ src/pages/faces/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Faces = () => {
+  return (
+    <View>
+      <Text>Faces Screen</Text>
+    </View>
+  );
+};
+
+export default Faces;
