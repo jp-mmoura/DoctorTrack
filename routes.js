1827942f2428673771f855f3cbdbfd1f0dd2e013
João Pedro <133026499+jp-mmoura@users.noreply.github.com>
2024-04-16 17:04:50 -0300

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import MapScreen from './MapScreen';
+import Home from './pages/home';
+import Faces from './pages/faces'; 
+
+const Tab = createBottomTabNavigator();
+
+export function Routes() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused) {
+              return <Ionicons size={size} color={color} name="home" />;
+            }
+            return <Ionicons size={size} color={color} name="home-outline" />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="faces"
+        component={Faces} // Updated component
+        options={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused) {
+              return <Ionicons size={size} color={color} name="happy" />;
+            }
+            return <Ionicons size={size} color={color} name="happy-outline" />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="map"
+        component={MapScreen}
+        options={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused) {
+              return <Ionicons size={size} color={color} name="map" />;
+            }
+            return <Ionicons size={size} color={color} name="map-outline" />;
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
